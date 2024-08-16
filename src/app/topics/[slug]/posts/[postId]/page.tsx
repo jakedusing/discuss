e858717dcfcd843255b002c6a5d031fd12970091
Jake Dusing <dusingjake@gmail.com>
2024-08-15 19:22:42 -0500

    add component streaming with suspense

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import { Suspense } from "react";
 import PostShow from "@/components/posts/post-show";
 import CommentList from "@/components/comments/comment-list";
 import CommentCreateForm from "@/components/comments/comment-create-form";
 import paths from "@/paths";
 import { fetchCommentsByPostId } from "@/db/queries/comments";
+import { divider } from "@nextui-org/react";
 
 interface PostShowPageProps {
   params: {
@@ -20,7 +22,9 @@ export default async function PostShowPage({ params }: PostShowPageProps) {
       <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
         {"< "}Back to {slug}
       </Link>
-      {<PostShow postId={postId} />}
+      <Suspense fallback={<div>Loading...</div>}>
+        {<PostShow postId={postId} />}
+      </Suspense>
       {<CommentCreateForm postId={postId} startOpen />}
       {<CommentList postId={postId} />}
     </div>
